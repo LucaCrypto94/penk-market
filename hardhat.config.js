7212c88ef6cacad475c6c319eb8f597ac6306cff
drewmooner <princewillroberts@gmail.com>
2025-08-26 22:38:22 +0100

    L2 contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,70 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+require('@nomicfoundation/hardhat-toolbox');
+require('@openzeppelin/hardhat-upgrades');
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
+    'pepu-v2-testnet-vn4qxxp9og': {
+      url: 'https://rpc-pepu-v2-testnet-vn4qxxp9og.t.conduit.xyz',
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 11155111,
     },
-    hardhat: {
-      chainId: 31337,
+    'pepu-v2-mainnet': {
+      url: 'https://rpc-pepu-v2-mainnet-0.t.conduit.xyz',
+      chainId: 97741,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    'pepu-mainnet': {
+      url: 'https://rpc-pepu-v2-mainnet-0.t.conduit.xyz',
+      chainId: 97741,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || '',
+      chainId: 11155111,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    mainnet: {
+      url: process.env.ETHEREUM_RPC_URL || '',
+      chainId: 1,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
   etherscan: {
     apiKey: {
-      sepolia: process.env.ETHERSCAN_API_KEY || "",
+      sepolia: process.env.ETHERSCAN_API_KEY,
+      mainnet: process.env.ETHERSCAN_API_KEY,
+      'pepu-v2-testnet-vn4qxxp9og': 'empty',
+      'pepu-v2-mainnet': 'empty',
+      'pepu-mainnet': 'empty',
     },
+    customChains: [
+      {
+        network: "pepu-v2-testnet-vn4qxxp9og",
+        chainId: 97740,
+        urls: {
+          apiURL: "https://explorer-pepu-v2-testnet-vn4qxxp9og.t.conduit.xyz/api",
+          browserURL: "https://explorer-pepu-v2-testnet-vn4qxxp9og.t.conduit.xyz:443"
+        }
+      },
+      {
+        network: "pepu-v2-mainnet",
+        chainId: 97741,
+        urls: {
+          apiURL: "https://pepuscan.com/api",
+          browserURL: "https://pepuscan.com/"
+        }
+      },
+      {
+        network: "pepu-mainnet",
+        chainId: 97741,
+        urls: {
+          apiURL: "https://pepuscan.com/api",
+          browserURL: "https://pepuscan.com/"
+        }
+      }
+    ]
   },
   paths: {
     sources: "./contracts",
